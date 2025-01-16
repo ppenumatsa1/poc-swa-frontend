1316e76e9a735ed0199f2e0cc59aaf4956651543
Praveen Penumatsa <praveen@test.com>
2025-01-16 13:56:52 -0600

    Add Front Door Route

--- src/config.js
+++ src/config.js
@@ -1,10 +1,16 @@
 const config = {
-    apiBaseUrl: 'https://poc-swa-backend-0115-avgnb4bwb5a9gcam.northcentralus-01.azurewebsites.net/api'
+    apiBaseUrl: 'https://poc-swa-fd-ep-fsg0gkhyava9cjbg.b01.azurefd.net/api'
 };
+
+// const config = {
+//     apiBaseUrl: 'https://poc-swa-backend-0115-avgnb4bwb5a9gcam.northcentralus-01.azurewebsites.net/api'
+// };
 
 // const config = {
 //     apiBaseUrl: 'http://localhost:8000/api'
 // };
+
+
 
 
 export default config;
